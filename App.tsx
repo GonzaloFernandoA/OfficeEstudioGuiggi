64623ab42ae4506fd56b3140967946296488b89e
ffern <ffern@PF9H9RY0H9TEID>
2026-01-16 18:55:38 -0300

    Se elimina campo rendundante. Todo para quitar Lugar del hecho.

--- App.tsx
+++ App.tsx
@@ -954,8 +954,7 @@ function App() {
                         <Section title="Datos del Siniestro">
                             <InputField label="Fecha del Hecho" name="siniestro.fechaHecho" type="date" value={formData.siniestro.fechaHecho} onChange={handleInputChange} onBlur={handleBlur} error={getNestedValue(errors, 'siniestro.fechaHecho')} required />
                             <InputField label="Hora Aproximada" name="siniestro.horaHecho" type="time" value={formData.siniestro.horaHecho} onChange={handleInputChange} onBlur={handleBlur} error={getNestedValue(errors, 'siniestro.horaHecho')} required />
-                            <InputField label="Lugar del Hecho" name="siniestro.lugarHecho" value={formData.siniestro.lugarHecho} onChange={handleInputChange} onBlur={handleBlur} helpText="Ej: Av. Rivadavia y Av. Callao" error={getNestedValue(errors, 'siniestro.lugarHecho')} required />
-                            <InputField label="Calles" name="siniestro.calles" value={formData.siniestro.calles} onChange={handleInputChange} onBlur={handleBlur} error={getNestedValue(errors, 'siniestro.calles')} />
+                            /*TODO, remover campo y que no rompa validacion*/<InputField label="Lugar del Hecho" name="siniestro.lugarHecho" value={formData.siniestro.lugarHecho} onChange={handleInputChange} onBlur={handleBlur} helpText="Ej: Av. Rivadavia y Av. Callao" error={getNestedValue(errors, 'siniestro.lugarHecho')} required />
                             <AddressRow
                                 calleName="siniestro.calles"
                                 calleValue={formData.siniestro.calles || ''}
